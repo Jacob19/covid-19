fd7fe8e30c3964f6f146a6198527fa91dd70c153
Jacob C F <jacob.c_f@nokia.com>
2020-03-28 03:13:15 +0530

    Condition to check for availability of data

--- data.py
+++ data.py
@@ -12,6 +12,7 @@ def extract_contents(row): return [x.text.replace('\n', '') for x in row]
 
 def get_stats():
     URL = 'https://www.mohfw.gov.in/'
+    
 
     SHORT_HEADERS = ['Sl. No.', 'State', 'Indian-Confirmed',
                      'Foreign-Confirmed', 'Cured', 'Death']
@@ -19,19 +20,25 @@ def get_stats():
     response = requests.get(URL).content
     soup = BeautifulSoup(response, 'html.parser')
 
-    stats = []
     cases = soup.find(id='cases')
-    all_table_rows = cases.find_all('tr')
-    all_table_rows.remove(all_table_rows[0])
-    all_table_rows.pop()
+    stats = []
+    
+    if cases:
+    
+        all_table_rows = cases.find_all('tr')
+        all_table_rows.remove(all_table_rows[0])
+        all_table_rows.pop()
 
-    for row in all_table_rows:
-        stat = extract_contents(row.find_all('td'))
-        if len(stat) == 5:
-            stat = ['', *stat]
-        stats.append({SHORT_HEADERS[i]: stat[i] for i in range(0, len(stat))})
+        for row in all_table_rows:
+            stat = extract_contents(row.find_all('td'))
+            if len(stat) == 5:
+                stat = ['', *stat]
+            stats.append({SHORT_HEADERS[i]: stat[i] for i in range(0, len(stat))})
 
-    # table = tabulate(stats, headers=SHORT_HEADERS)
-    # print(table)
+        # table = tabulate(stats, headers=SHORT_HEADERS)
+        # print(table)
 
-    return(stats)
+    return(stats)   
+
+
+get_stats()
